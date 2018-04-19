24d6d96d430554b5dcc804dc92bed8a16203a773
Vidovitsch <david.de.bekker@gmail.com>
2018-04-19 21:50:20 +0200

    Barebone models and services initialized

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const ClientGateway = require('./lib/gateways/clientGateway');
+const DealerGateway = require('./lib/gateways/dealerGateway');
+const LobbyGateway = require('./lib/gateways/lobbyGateway');
+const TableGateway = require('./lib/gateways/tableGateway');
+
+module.exports = (options) => {
+    this.client = () => {
+        return new ClientGateway();
+    };
+    this.dealer = () => {
+        return new DealerGateway();
+    };
+    this.lobby = () => {
+        return new LobbyGateway();
+    };
+    this.table = () => {
+        return new TableGateway();
+    };
+}
