b9580bc7d33550f13176937bf4f2ec8435410afa
Vidovitsch <david.de.bekker@gmail.com>
2018-04-19 22:00:36 +0200

    module init fix

--- index.js
+++ index.js
@@ -5,17 +5,12 @@ const DealerGateway = require('./lib/gateways/dealerGateway');
 const LobbyGateway = require('./lib/gateways/lobbyGateway');
 const TableGateway = require('./lib/gateways/tableGateway');
 
-module.exports = (options) => {
-    this.client = () => {
-        return new ClientGateway();
-    };
-    this.dealer = () => {
-        return new DealerGateway();
-    };
-    this.lobby = () => {
-        return new LobbyGateway();
-    };
-    this.table = () => {
-        return new TableGateway();
-    };
+function Gateway(options) {
+    this.options = options;
 }
+
+Gateway.prototype.client = () => {
+    return new ClientGateway();
+}
+
+module.exports = Gateway;
