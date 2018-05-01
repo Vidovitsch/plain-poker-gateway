fcb4a40dce389fb95309801f5d73e8b52e917b29
Vidovitsch <david.de.bekker@gmail.com>
2018-05-01 17:28:00 +0200

    Splitted gateway classes into amqpGateways and socketGateways

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const ClientGateway = require('./lib/gateways/clientGateway');
-const DealerGateway = require('./lib/gateways/dealerGateway');
-const LobbyGateway = require('./lib/gateways/lobbyGateway');
-const TableGateway = require('./lib/gateways/tableGateway');
+const ClientAmqpGateway = require('./lib/gateways/client/clientAmqpGateway');
+const ClientSocketGateway = require('./lib/gateways/client/clientSocketGateway');
+const DealerAmqpGateway = require('./lib/gateways/dealer/dealerAmqpGateway');
+const LobbyAmqpGateway = require('./lib/gateways/lobby/lobbyAmqpGateway');
+const LobbySocketGateway = require('./lib/gateways/lobby/lobbySocketGateway');
+const TableAmqpGateway = require('./lib/gateways/table/tableAmqpGateway');
 
-exports.createClientGateway = options => new ClientGateway(options);
+exports.getClientGateway = function getClientGateway(protocol, options) {
+  if (protocol === 'amqp') {
+    return new ClientAmqpGateway(options);
+  } else if (protocol === 'socket') {
+    return new ClientSocketGateway(options);
+  }
+  return new Error(`Protocol ${protocol} is invalid, use 'amqp' or 'socket instead'`);
+};
 
-exports.createDealerGateway = options => new DealerGateway(options);
+exports.getDealerGateway = function getDealerGateway(protocol, options) {
+  if (protocol === 'amqp') {
+    return new DealerAmqpGateway(options);
+  }
+  return new Error(`Protocol ${protocol} is invalid, use 'amqp' instead'`);
+};
 
-exports.createLobbyGateway = options => new LobbyGateway(options);
+exports.getLobbyGateway = function getLobbyGateway(protocol, options) {
+  if (protocol === 'amqp') {
+    return new LobbyAmqpGateway(options);
+  } else if (protocol === 'socket') {
+    return new LobbySocketGateway(options);
+  }
+  return new Error(`Protocol ${protocol} is invalid, use 'amqp' or 'socket instead'`);
+};
 
-exports.createTableGateway = options => new TableGateway(options);
+exports.getTableAmqpGateway = function getTableAmqpGateway(protocol, options) {
+  if (protocol === 'amqp') {
+    return new TableAmqpGateway(options);
+  }
+  return new Error(`Protocol ${protocol} is invalid, use 'amqp'`);
+};
